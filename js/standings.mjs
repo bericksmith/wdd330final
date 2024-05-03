5de004e8d4d28f1ebf9fd10cb3cb33afa6832b17
bericksmith <smi23001@byui.edu>
2024-05-03 10:54:14 -0700

    standings update

--- js/standings.mjs
+++ js/standings.mjs
@@ -5,9 +5,9 @@ const url = 'https://major-league-baseball-mlb.p.rapidapi.com/standings?year=202
 async function fetchStandings() {
     try {
         const response = await fetch(url, apiOptions);
-        const data = await response.json();  // Assuming the API returns JSON data
+        const data = await response.json();
         const display = document.getElementById('standingsContent');
-        display.textContent = JSON.stringify(data, null, 2);  // Displaying formatted JSON data
+        display.textContent = JSON.stringify(data, null, 2);
     } catch (error) {
         console.error('Failed to fetch data:', error);
         document.getElementById('standingsContent').textContent = 'Failed to load data.';
