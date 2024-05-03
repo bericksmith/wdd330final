4f61fe8bfd4895083131fb1b415f5949bec3cedd
bericksmith <smi23001@byui.edu>
2024-05-03 08:43:28 -0700

    updat main.js

--- js/news.mjs
+++ js/news.mjs
@@ -2,7 +2,7 @@ import { apiOptions } from './config.mjs';
 
 const url = 'https://major-league-baseball-mlb.p.rapidapi.com/news';
 
-export async function fetchNews() {
+async function fetchNews() {
     try {
         const response = await fetch(url, apiOptions);
         const data = await response.json();
