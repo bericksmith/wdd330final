193e7f49dc2194e7d10acbd167e51ffbad63e07f
bericksmith <smi23001@byui.edu>
2024-05-07 07:19:41 -0700

    main import changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import fetchSchedule from "./schedule.mjs";
-import fetchScoreboard from "./scoreboard.mjs";
-import fetchStandings from "./standings.mjs";
-import fetchNews from "./news.mjs";
-import fetchTeamInfo from "./teamInfo.mjs";
+import { fetchSchedule } from "./schedule.mjs";
+import { fetchScoreboard } from "./scoreboard.mjs";
+import { fetchStandings } from "./standings.mjs";
+import { fetchNews } from './news.mjs';
+import { fetchTeamInfo } from "./teamInfo.mjs";
 
 fetchSchedule();
 fetchScoreboard();
