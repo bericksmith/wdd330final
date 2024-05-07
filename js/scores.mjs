93b1419b1f53bcbbeb2d89f203c0c55cde43d470
bericksmith <smi23001@byui.edu>
2024-05-07 16:13:51 -0700

    scores css changes

--- js/scores.mjs
+++ js/scores.mjs
@@ -27,7 +27,7 @@ async function fetchScores() {
                     const statusDetail = competition.status.type.shortDetail || 'No Detail Available';
                     const gameElement = document.createElement('div');
                     gameElement.innerHTML = `
-                        <h2>${event.shortName} - ${statusDetail}</h2>
+                        <p><strong>${statusDetail}</strong></p>
                         <div style="display: flex; align-items: center; margin-bottom: 5px;">
                             <img src="${competitors[0].team.logo}" alt="${competitors[0].team.displayName}" style="height:20px; margin-right: 10px;">
                             <span><strong>${competitors[0].team.abbreviation}</strong>: ${competitors[0].score}</span>
