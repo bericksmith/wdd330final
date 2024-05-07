f21e633d26f1976deacacfa4055876f0b0ae67f4
bericksmith <smi23001@byui.edu>
2024-05-07 07:36:09 -0700

    teams

---
+++ js/teams.mjs
@@ -0,0 +1,16 @@
+const url = 'https://major-league-baseball-mlb.p.rapidapi.com/team-list';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b74c0e6687msh81dc7b82824316fp18b727jsn0b2d2b1fa9e0',
+		'X-RapidAPI-Host': 'major-league-baseball-mlb.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
