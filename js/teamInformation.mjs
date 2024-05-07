ed15d3b6328a10584cfc6ab40cd0ce79ae012b38
bericksmith <smi23001@byui.edu>
2024-05-07 12:13:57 -0700

    venue image css

--- js/teamInformation.mjs
+++ js/teamInformation.mjs
@@ -32,33 +32,32 @@ function formatTeamDetails(data) {
     const nextEventDate = formatDate(team.nextEvent[0].date);
 
     return `
-        <div style="display: flex; justify-content: space-between; margin-bottom: 20px;">
-            <div style="flex: 1; margin-right: 20px;">
-                <h2>${team.displayName}</h2>
-                <img src="${team.logos[0].href}" alt="${team.shortDisplayName}" style="width:100px; height:100px;">
-                <p><strong>Team Colors:</strong><p> 
-                <span style="display:inline-block; width:20px; height:20px; background-color:#${team.color};"></span>
-                <span style="display:inline-block; width:20px; height:20px; background-color:#${team.alternateColor};"></span>
-                <p><strong>Current Record:</strong> ${team.record.items[0].summary}</p>
-                <div>
-                    <h3>Links</h3>
-                    <p><a href="${team.links[1].href}" target="_blank">Team Roster - 2024</a></p>
-                    <p><a href="${team.links[2].href}" target="_blank">Team Schedule -2024</a></p>
- 
-                </div>
-            </div>
-            <div style="flex: 1;">
-                <div>
-                    <h3>Home Field - <i>${team.franchise.venue.fullName}</i></h3>
-                    <img src="${team.franchise.venue.images[0].href}" alt="Venue Image" style="width:300px; height:180px;">
-                    <p>Location: ${team.franchise.venue.address.city}, ${team.franchise.venue.address.state}</p>
-                </div>
-                <div>
-                    <h3>Next Game - ${nextEventDate}</h3>
-                    <p>${team.nextEvent[0].name}</p>
-                    <p><a href="${team.links[5].href}" target="_blank">Get Tickets!</a></p>
-                </div>
-            </div>
+    <div class="flex-container">
+    <div class="team-section">
+        <h2>${team.displayName}</h2>
+        <img src="${team.logos[0].href}" alt="${team.shortDisplayName}" class="team-img">
+        <p><strong>Team Colors:</strong><p>
+        <span class="team-colors" style="background-color:#${team.color};"></span>
+        <span class="team-colors" style="background-color:#${team.alternateColor};"></span>
+        <p><strong>Current Record:</strong> ${team.record.items[0].summary}</p>
+        <div>
+            <h3>Links</h3>
+            <p><a href="${team.links[1].href}" target="_blank">Team Roster - 2024</a></p>
+            <p><a href="${team.links[2].href}" target="_blank">Team Schedule - 2024</a></p>
         </div>
+    </div>
+    <div class="venue-section">
+        <div>
+            <h3>Home Field - <i>${team.franchise.venue.fullName}</i></h3>
+            <img src="${team.franchise.venue.images[0].href}" alt="Venue Image" class="venue-img">
+            <p>Location: ${team.franchise.venue.address.city}, ${team.franchise.venue.address.state}</p>
+        </div>
+        <div>
+            <h3>Next Game - ${nextEventDate}</h3>
+            <p>${team.nextEvent[0].name}</p>
+            <p><a href="${team.links[5].href}" target="_blank">Get Tickets!</a></p>
+        </div>
+    </div>
+</div>
     `;
 }
