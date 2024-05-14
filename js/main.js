98058ffcc0fce8b0a79995e0e333c68b8feb2900
bericksmith <smi23001@byui.edu>
2024-05-14 16:11:16 -0700

    add weather

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ import { displayTeamInfo } from './teamInformation.mjs';
 import { fetchScores } from './scores.mjs';
 import { fetchNews } from './news.mjs';
 import { fetchTeamsAndDisplay } from './teams.mjs';
+import { fetchWeather } from './weather.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
     updateVisitInfo();
     fetchScores().catch(console.error);
     fetchNews().catch(console.error);
     fetchTeamsAndDisplay().catch(console.error);
+    fetchWeather().catch(console.error);
     displayTeamInfo('15'); // Start with braves if no selection
 });
 
