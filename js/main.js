981e2734885a2dd8d11b2f523cda74569111b5c9
bericksmith <smi23001@byui.edu>
2024-05-14 13:48:14 -0700

    local storage last visit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ import { fetchNews } from './news.mjs';
 import { fetchTeamsAndDisplay } from './teams.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchScores();
-    fetchNews();
-    fetchTeamsAndDisplay();
-    displayTeamInfo('15'); // ID for the Braves
+    updateVisitInfo();
+    fetchScores().catch(console.error);
+    fetchNews().catch(console.error);
+    fetchTeamsAndDisplay().catch(console.error);
+    displayTeamInfo('15'); // Start with braves if no selection
 });
+
+function updateVisitInfo() {
+    localStorage.setItem('lastVisit', new Date().toLocaleString()); 
+    const lastVisit = localStorage.getItem('lastVisit');
+    const footer = document.getElementById('visitInfo');
+    footer.textContent = `Last visit: ${lastVisit}`;
+}
