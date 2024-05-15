bdee9679861c4ee0e26dc782282bcc122fe53f35
bericksmith <smi23001@byui.edu>
2024-05-14 18:23:47 -0700

    yogi

--- js/berra.js
+++ js/berra.js
@@ -2,14 +2,29 @@ function fetchJSONData() {
     fetch("./json/yogi-berra.json")
         .then((res) => {
             if (!res.ok) {
-                // Corrected the use of 'new Error'
                 throw new Error(`HTTP error! Status: ${res.status}`);
             }
             return res.json();
         })
-        .then((data) => 
-              console.log(data))
+        .then((data) => {
+            if (data.length > 0) {
+                // Select a random quote from the array
+                const randomQuote = data[Math.floor(Math.random() * data.length)];
+                displayQuote(randomQuote);
+            } else {
+                console.log("No quotes available");
+            }
+        })
         .catch((error) => 
                console.error("Unable to fetch data:", error));
 }
-fetchJSONData();
+
+function displayQuote(quote) {
+    // Assuming there's a div with id="quoteDisplay" in your HTML
+    const quoteElement = document.getElementById('quoteDisplay');
+    if (quoteElement) {
+        quoteElement.innerHTML = `<p>"${quote.text}" - ${quote.author}</p>`;
+    } else {
+        console.error("No element with id 'quoteDisplay' found in HTML.");
+    }
+}
