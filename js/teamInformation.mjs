d9dca72ba6189435e4e675ce8e9b8df624aa0e74
bericksmith <smi23001@byui.edu>
2024-05-14 17:33:41 -0700

    css

--- js/teamInformation.mjs
+++ js/teamInformation.mjs
@@ -81,7 +81,7 @@ function formatTeamDetails(data) {
                 </div>
                 <div class="ticket-container">
                     <h3>Next Game</h3>
-                    <p style="margin: 5px"><strong>${team.nextEvent[0].name}</strong></p>
+                    <p style="margin: 5px"><i><strong>${team.nextEvent[0].name}</strong></i></p>
                     <p style="margin-top: 5px; margin-bottom: 10px;">${nextEventDate}</p>
                     <div id="weatherDisplay"></div>
                     <p><a href="${team.links[5].href}" class="ticketbutton" target="_blank">Get ${team.displayName} Tickets!</a></p>
