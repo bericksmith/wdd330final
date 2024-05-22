5b38c1810f1fa4c348893ece1877e1e0bfdf2208
bericksmith <smi23001@byui.edu>
2024-05-22 13:52:31 -0700

    news.js

--- js/news.mjs
+++ js/news.mjs
@@ -21,10 +21,7 @@ export async function fetchNews() {
 }
 
 function formatNewsItem(item) {
-    return `
-        <div class="news-item">
-            <a href="${item.link}" class="external-link" target="_blank">${item.headline}</a> - ESPN.com <strong> <i> ${new Date(item.published || '').toLocaleString()}</i></strong> 
-        </div>
+    return `<a href="${item.link}" class="external-link" target="_blank">${item.headline}</a> - ESPN.com <strong> <i> ${new Date(item.published || '').toLocaleString()}</i></strong><br> 
     `;
 }
 
